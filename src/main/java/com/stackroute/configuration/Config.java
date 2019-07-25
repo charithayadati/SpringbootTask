103574ba114ddeb086069862395d6c223f189aaf
charithayadati <charithay522@gmail.com>
2019-07-25 11:02:19 +0530

    nineth task

--- src/main/java/com/stackroute/configuration/Config.java
+++ src/main/java/com/stackroute/configuration/Config.java
@@ -9,13 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Tag;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 
 
@@ -31,23 +24,7 @@ public class Config {
         registrationBean.addUrlMappings("/console/*");
         return registrationBean;
     }
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors
-                        .basePackage("com.stackroute.Muzix.controller"))
-                .paths(PathSelectors.regex("/.*"))
-                .build().apiInfo(apiEndPointsInfo()).tags(new Tag("Track Controller", "CURD Operations"));
-    }
-    private ApiInfo apiEndPointsInfo() {
-        return new ApiInfoBuilder().title("Spring Boot REST API")
-                .description("Track Management REST API")
 
-                .license("Apache 2.0")
-                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-                .version("1.0.0")
-                .build();
-    }
     @EventListener
     public void handleContextRefreshEvent(ContextRefreshedEvent cfr) {
         try {
