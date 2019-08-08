284e5314c6c330d419b49fb9b07abdc4d17a0905
charithayadati <51942956+charithayadati@users.noreply.github.com>
2019-08-08 09:30:31 +0530

    Update Track.java

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -2,37 +2,28 @@ package com.stackroute.domain;
 
 
 
+
+
+   
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Track {
-//implementing getter and setter methods for the literals
 
+
+    @Id
     private int id;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     private String name;
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
     private String comment;
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
 
 }
